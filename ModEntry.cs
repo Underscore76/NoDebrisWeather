f7e6d549f9b0911292a20be3e707c21272c1903c
Underscore76 <theunderscore76@gmail.com>
2024-01-02 17:03:56 -0600

    init commit

---
+++ ModEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using StardewModdingAPI;
+using StardewValley;
+
+namespace NoDebrisWeather
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.GameLoop.UpdateTicked += GameLoop_UpdateTicked;
+        }
+
+        private void GameLoop_UpdateTicked(object sender, StardewModdingAPI.Events.UpdateTickedEventArgs e)
+        {
+            if (!Context.IsWorldReady)
+                return;
+
+            if (Game1.debrisWeather != null)
+            {
+                Game1.debrisWeather.Clear();
+            }
+        }
+    }
+}
+
